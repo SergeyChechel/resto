b0ce35ea1ea143e6ef4f9fed410cb51d84b8bf5d
Sergey <chechelsk@gmail.com>
2020-12-25 09:33:50 +0200

    finish

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,15 +2,25 @@ import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 
 import Background from './food-bg.jpg';
 
-const App = () => {
+const App = ({items}) => {
+
+    let total = 0;
+    if (items.length > 0) {
+        items.forEach(item => {
+            const sum = item.price * item.quantity;
+            total += sum;
+        });
+    }
+    
     return (
         <Router>
             <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-                <AppHeader total={50}/>
+                <AppHeader total={total}/>
                 <Route path='/' exact component={MainPage}/>
                 <Route path='/menu' component={MainPage}/>
                 <Route path='/cart' exact component={CartPage}/>
@@ -20,4 +30,15 @@ const App = () => {
     )
 }
 
-export default App;
+const mapStateToProps = ({items}) => {
+    return {
+       items 
+    }
+};
+
+// const mapDispatchToProps = {
+//     deleteFromCart
+// }
+
+
+export default connect(mapStateToProps)(App);
